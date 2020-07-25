4db110092c79ce7b8ccbef152944bc61c71790cd
Harshita Goswami <goswamiharshita0802@gmail.com>
2020-07-25 12:40:31 +0530

    Initial commit

---
+++ color.js
@@ -0,0 +1,12 @@
+<html>
+<head>
+  <script language = “javascript"> 
+    function ChangeColor() { 
+      document.body.bgColor="yellow" ;
+    } 
+  </script> 
+</head> 
+<body onclick="ChangeColor()"> 
+  Click on this document! 
+</body> 
+</html>
